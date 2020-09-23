e73952dbd13f31f3e6d741d4b5fe604616bc5429
ronngo <ronngomn23@gmail.com>
2020-09-23 21:53:21 +0700

    first commit

---
+++ Dog.cs
@@ -0,0 +1,24 @@
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace zoomanagementsystem
+{
+    public class Dog : Animal
+    {
+
+        public override void Speak()
+        {
+            Console.WriteLine("Gau Gau");
+        }
+        public Dog() : base(){}
+        public override void ViewInfo()
+        {
+            base.ViewInfo();
+        }
+        
+        public Dog(string Name, int Age, string Description) : base(Name, Age, Description)
+        {
+        }
+    }
+}
